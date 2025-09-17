c7b2d9cee7dc704d40be3e0aec02cebe6f8a4f05
Orhan Seyfi <novospire@gmail.com>
2025-09-17 21:33:54 +0300

    Fix Vite base path for Vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // ✅ Vercel için relative asset path
   build: {
     outDir: 'dist',
     sourcemap: false,
